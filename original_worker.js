21e523202dacb7210140096691d305619d731a8e
Steven Fourie <steven.fouriex@gmail.com>
2025-11-14 22:42:31 +0100

    add original worker

---
+++ original_worker.js
@@ -0,0 +1,132 @@
+
+addEventListener('fetch', event => {
+  try {
+    const request = event.request;
+    //return event.respondWith(handleRequest(event)) 
+    if (request.method.toUpperCase() === 'GET')
+      return event.respondWith(handleRequest(event)) 
+    else
+      return event.respondWith(fetch(request));
+  } catch (e) {
+  return event.respondWith(new Response('Error thrown ' + e.message));
+}
+
+})
+
+class MpshsoftwareHandler {
+element(element) {
+  element.append(`<script src="https://cdn.popupsmart.com/bundle.js" data-id="216989" async defer></script>`, {html: true});
+  element.append(`<script defer data-domain="mpshsoftware.com" data-api="https://mpshsoftware.com/psb/api/event" src="https://mpshsoftware.com/psb/js/psb.js"></script>`, {html: true});
+  element.append(`<script data-domain="mpshsoftware.com" data-url="https://mpshsoftware.com/psb/api/event" src="https://mpsh.fra1.cdn.digitaloceanspaces.com/singularity/singularity.js"></script>`, {html: true});
+  element.append(`<script src="https://cdn.jsdelivr.net/gh/manucaralmo/GlowCookies@3.1.6/src/glowCookies.min.js"></script>`, {html: true});
+  element.append(`<script>glowCookies.start('en', {style: 2, 
+    hideAfterClick: true,
+    bannerLinkText: '  ',
+    acceptBtnText: 'Accept',
+    bannerDescription: 'We use our own and third-party cookies to personalize content and to analyze web traffic. Read more about our <a href="https://mpsh.fra1.cdn.digitaloceanspaces.com/policy/CookiePolicy.html" target="_blank">Cookie Policy</a> and <a href="https://mpsh.fra1.cdn.digitaloceanspaces.com/policy/PrivacyNotice.html" target="_blank">Privacy Policy.</a>'});</script>`, {html: true});
+}
+}
+
+class MpshecosystemHandler {
+element(element) {
+  element.append(`<script src="https://cdn.popupsmart.com/bundle.js" data-id="216989" async defer></script>`, {html: true});
+  element.append(`<script defer data-domain="mpshecosystem.com" data-api="https://mpshecosystem.com/psb/api/event" src="https://mpshecosystem.com/psb/js/psb.js"></script>`, {html: true});
+  element.append(`<script data-domain="mpshecosystem.com" data-url="https://mpshecosystem.com/psb/api/event" src="https://mpsh.fra1.cdn.digitaloceanspaces.com/singularity/singularity.js"></script>`, {html: true});
+    element.append(`<script src="https://cdn.jsdelivr.net/gh/manucaralmo/GlowCookies@3.1.6/src/glowCookies.min.js"></script>`, {html: true});
+    element.append(`<script>glowCookies.start('en', {style: 2, 
+    hideAfterClick: true,
+    bannerLinkText: '  ',
+    acceptBtnText: 'Accept',
+    bannerDescription: 'We use our own and third-party cookies to personalize content and to analyze web traffic. Read more about our <a href="https://mpsh.fra1.cdn.digitaloceanspaces.com/policy/CookiePolicy.html" target="_blank">Cookie Policy</a> and <a href="https://mpsh.fra1.cdn.digitaloceanspaces.com/policy/PrivacyNotice.html" target="_blank">Privacy Policy.</a>'});</script>`, {html: true});
+}
+}
+
+class HomemarketplacesuperheroesHandler {
+element(element) {
+  element.append(`<script defer data-domain="home.marketplacesuperheroes.com" data-api="https://mpshecosystem.com/psb/api/event" src="https://mpshecosystem.com/psb/js/psb.js"></script>`, {html: true});
+    element.append(`<script src="https://cdn.jsdelivr.net/gh/manucaralmo/GlowCookies@3.1.6/src/glowCookies.min.js"></script>`, {html: true});
+    element.append(`<script>
+    setTimeout(function() {      
+      glowCookies.start('en', {style: 2, 
+    hideAfterClick: true,
+    bannerLinkText: '  ',
+    acceptBtnText: 'Accept',
+    bannerDescription: 'We use our own and third-party cookies to personalize content and to analyze web traffic. Read more about our <a href="https://mpsh.fra1.cdn.digitaloceanspaces.com/policy/CookiePolicy.html" target="_blank">Cookie Policy</a> and <a href="https://mpsh.fra1.cdn.digitaloceanspaces.com/policy/PrivacyNotice.html" target="_blank">Privacy Policy.</a>'});
+    }, 1000);
+    </script>`, {html: true});
+}
+}
+
+class FoursproductgauntletHandler {
+element(element) {
+  element.append(`<script defer data-domain="4sproductgauntlet.com" data-api="https://mpshecosystem.com/psb/api/event" src="https://mpshecosystem.com/psb/js/psb.js"></script>`, {html: true});
+  element.append(`<script data-domain="4sproductgauntlet.com" data-url="https://mpshecosystem.com/psb/api/event" src="https://mpsh.fra1.cdn.digitaloceanspaces.com/singularity/singularity.js"></script>`, {html: true});
+    element.append(`<script src="https://cdn.jsdelivr.net/gh/manucaralmo/GlowCookies@3.1.6/src/glowCookies.min.js"></script>`, {html: true});
+    element.append(`<script>glowCookies.start('en', {style: 2, 
+    hideAfterClick: true,
+    bannerLinkText: '  ',
+    acceptBtnText: 'Accept',
+    bannerDescription: 'We use our own and third-party cookies to personalize content and to analyze web traffic. Read more about our <a href="https://mpsh.fra1.cdn.digitaloceanspaces.com/policy/CookiePolicy.html" target="_blank">Cookie Policy</a> and <a href="https://mpsh.fra1.cdn.digitaloceanspaces.com/policy/PrivacyNotice.html" target="_blank">Privacy Policy.</a>'});</script>`, {html: true});
+}
+}
+
+class ThesuperherouniversityHandler {
+element(element) {
+  element.append(`<script defer data-domain="thesuperherouniversity.com" data-api="https://mpshecosystem.com/psb/api/event" src="https://mpshecosystem.com/psb/js/psb.js"></script>`, {html: true});
+  element.append(`<script data-domain="thesuperherouniversity.com" data-url="https://mpshecosystem.com/psb/api/event" src="https://mpsh.fra1.cdn.digitaloceanspaces.com/singularity/singularity.js"></script>`, {html: true});
+    element.append(`<script src="https://cdn.jsdelivr.net/gh/manucaralmo/GlowCookies@3.1.6/src/glowCookies.min.js"></script>`, {html: true});
+    element.append(`<script>glowCookies.start('en', {style: 2, 
+    hideAfterClick: true,
+    bannerLinkText: '  ',
+    acceptBtnText: 'Accept',
+    bannerDescription: 'We use our own and third-party cookies to personalize content and to analyze web traffic. Read more about our <a href="https://mpsh.fra1.cdn.digitaloceanspaces.com/policy/CookiePolicy.html" target="_blank">Cookie Policy</a> and <a href="https://mpsh.fra1.cdn.digitaloceanspaces.com/policy/PrivacyNotice.html" target="_blank">Privacy Policy.</a>'});</script>`, {html: true});
+}
+}
+
+async function handleRequest(event) {
+const request = event.request;
+const cacheUrl = new URL(request.url);
+
+  // Construct the cache key from the cache URL
+//const cacheKey = await sha256(cacheUrl.toString());
+const cacheKey = new Request(cacheUrl.toString(), request);
+const cache = caches.default;
+let response = await cache.match(cacheKey);
+
+if (!response) {
+  console.log(
+    `Response for request url: ${request.url} not present in cache. Fetching and caching request.`
+  );
+  response = await fetch(request)
+  response = new Response(response.body, response);
+  if (request.url.includes('mpshecosystem.com')) {
+      console.log("Injecting to mpshecosystem.com")
+    response = new HTMLRewriter().on("head", new MpshecosystemHandler()).transform(response)
+  }
+
+  if (request.url.includes('mpshsoftware.com')) {
+      console.log("Injecting to mpshsoftware.com")
+    response = new HTMLRewriter().on("head", new MpshsoftwareHandler()).transform(response)
+  }
+
+  if (request.url.includes('home.marketplacesuperheroes.com')) {
+      console.log("Injecting to marketplacesuperheroes.com")
+      response = new HTMLRewriter().on("head", new HomemarketplacesuperheroesHandler()).transform(response)
+  }
+
+  if (request.url.includes('4sproductgauntlet.com')) {
+      console.log("Injecting to 4sproductgauntlet.com")
+    response = new HTMLRewriter().on("head", new FoursproductgauntletHandler()).transform(response)
+  }
+
+  if (request.url.includes('thesuperherouniversity.com')) {
+      console.log("Injecting to thesuperherouniversity.com")
+    response = new HTMLRewriter().on("head", new ThesuperherouniversityHandler()).transform(response)
+  }
+  response.headers.append('Cache-Control', 's-maxage=10');
+  event.waitUntil(cache.put(cacheKey, response.clone()));
+} else {
+  console.log(`Cache hit for: ${request.url}.`);
+}
+return response;
+}
